4a8e7f2b2a546f87baa2f4f6ff70142828769d14
EsnNfl <muhammadehsannaufal@gmail.com>
2023-08-09 17:23:26 +0800

    fix demo js

--- app/Views/templates/main.php
+++ app/Views/templates/main.php
@@ -78,7 +78,7 @@
     <!-- AdminLTE App -->
     <script src="<?= base_url('assets/'); ?>dist/js/adminlte.js"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="<?= base_url('assets/'); ?>dist/js/demo.js"></script>
+    <!-- <script src="<?= base_url('assets/'); ?>dist/js/demo.js"></script> -->
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="<?= base_url('assets/'); ?>dist/js/pages/dashboard.js"></script>
 </body>
